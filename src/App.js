6283cf16f73f162d968dd99adf71a6d2cd1570b6
Ryan Szczepanski <rszczepanski@comcast.net>
2021-10-10 21:01:09 -0400

    allowed app to have many pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import {useState} from 'react'
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 //Components
-import NavBar from './components/NavBar/index.js';
-import Work from './components/Work/index.js'
-import AboutMe from './components/AboutMe/index.js'
-import Contact from './components/Contact/index.js'
+import Header from "./components/Header/index.js";
+import Work from "./components/Work/index.js";
+import AboutMe from "./components/AboutMe/index.js";
+import Contact from "./components/Contact/index.js";
 //Jsons
-import Projects from './jsons/projects.json'
-import AboutMeJson from './jsons/aboutme.json'
+import Projects from "./jsons/projects.json";
+import AboutMeJson from "./jsons/aboutme.json";
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
+  const [pageSelected, setPageSelected] = useState("home");
   return (
-  <>
-    <NavBar setContactSelected={setContactSelected}/>
-    <div className="content">
-      {!contactSelected ? (
-        <>
-          <AboutMe image={AboutMeJson.image} body={AboutMeJson.body}/>
-          <Work work={Projects}/>
-        </>
-      ) : (
-        <Contact/>
-      )}
-    </div>
-  </>
+    <>
+      <Header setPageSelected={setPageSelected} />
+      <div className="content">
+        {pageSelected == "home" ||
+        pageSelected == "about" ||
+        pageSelected == "portfolio" ? (
+          <>
+            <AboutMe image={AboutMeJson.image} body={AboutMeJson.body} />
+            <Work work={Projects} />
+          </>
+        ) : pageSelected == "contact" ? (
+          <Contact />
+        ) : (
+          <>
+          </>
+          //<Resume />
+        )}
+      </div>
+    </>
   );
 }
 
